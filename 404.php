26e72318f2a349642d40b499c404b443519d1b0e
pavelleonenko007 <pavel.leonenko374@gmail.com>
2025-09-15 15:24:17 +0300

    fix: 404 icons

--- 404.php
+++ 404.php
@@ -4,36 +4,33 @@ Template name: 404-page
 */
 
 get_header(
-    null,
-    array(
-        'data-wf-page'                  => '6720c60d7d5faf3e7ea1ac8d',
-        'barba-container-extra-classes' => array(
-            'home-page',
-        ),
-        'namespace'                     => 'home',
-    )
+	null,
+	array(
+		'data-wf-page'                  => '6720c60d7d5faf3e7ea1ac8d',
+		'barba-container-extra-classes' => array(
+			'home-page',
+		),
+		'namespace'                     => 'home',
+	)
 ); ?>
-    <div id="barba-wrapper" class="wrapper">
-        <div class="barba-container white-top">
-            <section class="section _404-page">
-                <a href="#" class="indirm-line w-inline-block">
-                    <div class="marq"><div class="marq-line">
-                            <div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div></div><div class="marq-line"><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div></div><div class="marq-line"><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div></div><div class="marq-line"><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div><div class="indirm-line_div"><div>распродажа до 70%</div><div class="indirm-line_romb"></div></div></div></div></a>
-                <?php get_template_part( 'components/navbar' ); ?>
-                <div class="container container-fw _404-page-container">
-                    <div class="page-top-copy _404-page"><img src="<?php echo get_template_directory_uri(); ?>/images/672b7395d7c0f40bf0140252_Group20647.svg" loading="lazy" alt class="image-3">
-                        <h1 class="p-64-64 h404">404</h1>
-                        <div class="p-16-20 p404">к сожалению, страница не найдена</div><a href="/" class="btn in-single-btn p-t-30 w-inline-block"><div>вернуться на главную</div></a></div>
-                </div>
-            </section>
-        </div>
-    </div>
-    <div class="ui"><a href="#" class="btn w-inline-block"><div>This is some text inside of a div block.</div></a><a href="#" class="btn in-slider-btn w-inline-block"><div>перейти в каталог</div></a><a href="#" class="btn fast-link-btn w-inline-block"><div class="fast-link-omg-mom"><img src="<?php echo get_template_directory_uri(); ?>/images/6720cf266f1f5059b3ae029d_Group20192.avif" loading="lazy" alt class="img-60w"></div><div>сияй</div></a><a href="#" class="btn in-slider-btn purple-color w-inline-block"><div>подробнее</div></a><a href="#" class="w-inline-block"></a><a id="w-node-_6d0e6280-7f82-d571-8601-b6d6f3ab2482-f3ab2472" href="#" class="btn btn-with-arrow w-inline-block"><div>Смотреть все</div><img src="<?php echo get_template_directory_uri(); ?>/images/6720d17cfb5622b535a21354_Arrow20Down.svg" loading="eager" alt class="img-arrow"></a><a id="w-node-_4666dd82-d674-9aae-955c-43b4bc3b8fc9-f3ab2472" href="#" class="btn grey-border_btn w-inline-block"><div class="p-12-12">атлас</div></a>
-        <div id="w-node-b85003fd-ed2f-f829-636a-40adc3252253-f3ab2472" class="rich-faq w-richtext">
-            <p>Консультант проводит в зону кафе, где за чашкой кофе узнает когда  свадьба, тематика и предпочтения по платью, далее примеряем выбранные  модели и среди всех выбираем наиболее понравившиеся.</p>
-        </div>
-    </div>
-    <!-- FOOTER CODE -->
+	<div id="barba-wrapper" class="wrapper">
+		<div class="barba-container white-top">
+			<section class="section _404-page">
+				<?php get_template_part( 'components/navbar' ); ?>
+				<div class="container container-fw _404-page-container">
+					<div class="page-top-copy _404-page"><img src="<?php echo get_template_directory_uri(); ?>/images/672b7395d7c0f40bf0140252_Group20647.svg" loading="lazy" alt class="image-3">
+						<h1 class="p-64-64 h404">404</h1>
+						<div class="p-16-20 p404">к сожалению, страница не найдена</div>
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="btn in-single-btn p-t-30 w-inline-block">
+							<div>вернуться на главную</div>
+						</a>
+					</div>
+				</div>
+			</section>
+		</div>
+	</div>
+	<?php get_template_part( 'components/icons' ); ?>
+	<!-- FOOTER CODE -->
 <?php wp_footer(); ?>
 </body>
 </html>
