de00016a0728628db7d9520adeea31ae7f5aa8d6
pavelleonenko007 <pavel.leonenko374@gmail.com>
2025-09-21 19:44:01 +0300

    fix: faq dropdown arrow

--- components/faq-dropdown.php
+++ components/faq-dropdown.php
@@ -13,7 +13,7 @@ global $post;
 	<div class="faq-top">
 		<div class="p-20-20 w500"><?php the_title(); ?></div>
 		<div class="faq-svg w-embed">
-			<svg width="10" height="6" viewbox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<svg width="10" height="6" viewbox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg" style="width: 10rem; height: 6rem;">
 				<path fill-rule="evenodd" clip-rule="evenodd" d="M4.28598 5.24977L0 0.74993L0.714289 0L5.00027 4.49984L9.28571 0.000560648L10 0.750491L4.99998 6L4.28569 5.25007L4.28598 5.24977Z" fill="black"></path>
 			</svg>
 		</div>
