42df7dcbced51d336a42639b2aa9f06aae1a80d5
pavelleonenko007 <pavel.leonenko374@gmail.com>
2025-10-02 10:51:49 +0300

    fix: remove php notices

--- components/review-card.php
+++ components/review-card.php
@@ -45,43 +45,6 @@ if ( empty( $image_carousel ) && ! has_post_thumbnail() ) {
 				</div>
 			</div>
 		</div>
-		<!-- <div data-delay="4000" data-animation="slide" class="slider-oyziv w-slider" data-autoplay="false" data-easing="ease" data-hide-arrows="false" data-disable-swipe="false" data-autoplay-limit="0" data-nav-spacing="3" data-duration="500" data-infinite="true">
-			<div class="slider-oyziv_mask w-slider-mask">
-				<?php
-				if ( has_post_thumbnail() ) :
-					?>
-					<div class="w-slide">
-						<div class="slider-oyziv_img-mom">
-							<?php the_post_thumbnail( 'fullhd', array( 'class' => 'img-cover' ) ); ?>
-						</div>
-					</div>
-				<?php endif; ?>
-				<?php
-				foreach ( $image_carousel as $image_carousel_item ) :
-					?>
-					<div class="w-slide">
-						<div class="slider-oyziv_img-mom">
-							<?php echo wp_get_attachment_image( $image_carousel_item['image'], 'fullhd', false, array( 'class' => 'img-cover' ) ); ?>
-						</div>
-					</div>
-				<?php endforeach; ?>
-			</div>
-			<div class="slider-oyziv_l w-slider-arrow-left">
-				<div class="pag-svg w-embed">
-					<svg width="6" height="10" viewbox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path fill-rule="evenodd" clip-rule="evenodd" d="M0.750232 4.28598L5.25007 0L6 0.714289L1.50016 5.00027L5.99944 9.28571L5.24951 10L0 4.99998L0.74993 4.28569L0.750232 4.28598Z" fill="white"></path>
-					</svg>
-				</div>
-			</div>
-			<div class="slider-oyziv_r w-slider-arrow-right">
-				<div class="pag-svg w-embed">
-					<svg width="6" height="10" viewbox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path fill-rule="evenodd" clip-rule="evenodd" d="M5.24977 4.28598L0.74993 0L0 0.714289L4.49984 5.00027L0.000560648 9.28571L0.750491 10L6 4.99998L5.25007 4.28569L5.24977 4.28598Z" fill="white"></path>
-					</svg>
-				</div>
-			</div>
-			<div class="slider-oyziv_nav w-slider-nav w-round w-num"></div>
-		</div> -->
 	<?php elseif ( has_post_thumbnail() ) : ?>
 		<img src="<?php echo esc_url( get_the_post_thumbnail_url( $post, 'fullhd' ) ); ?>" loading="lazy" alt class="img-fw">
 	<?php endif; ?>
